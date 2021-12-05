679ec48d0f14501c3966e98e56044ca4cd904e18
mirzanuta <mirzanuta@yahoo.com>
2021-12-05 22:20:30 +0400

    correct not available data case

--- ExchangeRates/Program.cs
+++ ExchangeRates/Program.cs
@@ -11,3 +11,7 @@ Console.WriteLine(currResponse?.ToString());
 var dt = "2021-11-30";
 currResponse = apiclient.GetRatesByDate(dt, arr);
 Console.WriteLine(currResponse?.ToString());
+
+string[] arr1 = {};
+currResponse = apiclient.GetLatestRates(arr1);
+Console.WriteLine(currResponse?.ToString());
